ef5b4fa4dd166f1da6c542bd3ade9541579ad241
DanielZhu <enterzhu@gmail.com>
2016-04-25 13:08:13 +0800

    Update new mock data

---
+++ ext/src/mock/signinSuccess.js
@@ -0,0 +1,15 @@
+{
+    "status": 0,
+    "msg": "msg",
+    "data": {
+        "result": {
+            "userId": 123, //我的id
+            "sign": 1,  //是否已领积分，1表示今天已领 0未领
+            "pointsTotal": 12, //我的积分分值
+            "pointsToday": 3, //今天可领积分, 如果今天已领，则为0
+            "days": 2, //已签到几天
+            "nextDays": 6, //提示再签几天可领下一轮积分，详情见mrd描述
+            "nextPoints": 10 //再签N天可领分值
+        }
+    }
+}
